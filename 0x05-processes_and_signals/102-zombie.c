9032635eab676969dc8361a99db1c975f993c982
David Kwan <dwkwan1989@gmail.com>
2019-06-25 00:12:21 +0000

    Add task 11

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+/**
+ * createzombie - creates a zombie process
+ *
+ * Return: nothing
+ */
+void createzombie(void)
+{
+	pid_t my_pid;
+
+	if (fork() == 0)
+	{
+		my_pid = getpid();
+		printf("Zombie process created, %u\n", my_pid);
+		exit(0);
+	}
+}
+/**
+ * infinite_while - creates an infinite loop
+ *
+ * Return: nothing
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+/**
+ * main - passes execution control
+ *
+ * Return: nothing
+ */
+void main(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 5)
+	{
+		createzombie();
+		i++;
+	}
+	infinite_while();
+}

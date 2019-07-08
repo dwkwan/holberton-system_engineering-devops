81dad92dbb6c6034704a1ab6bed40c747f33fb36
David Kwan <dwkwan1989@gmail.com>
2019-07-08 21:50:24 +0000

    Refactor

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,7 +1,2 @@
 #!/usr/bin/env ruby
-print ARGV[0].scan(/(?<=from:).*?(?=\])/).join
-print(",")
-print ARGV[0].scan(/(?<=to:).*?(?=\])/).join
-print(",")
-print ARGV[0].scan(/(?<=flags:).*?(?=\])/).join
-print("\n")
+puts ARGV[0].scan(/(?<=from:|to:|flags:).*?(?=\])/).join(",")

5d7719d42394463f0e920cb7dbb6cac52737a870
David Kwan <dwkwan1989@gmail.com>
2019-08-05 23:17:37 +0000

    Add tasks and README.md

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+#Creates Sets up client SSH configuration file
+file_line { 'ssh_config':
+    ensure   => present,
+    path     => '/etc/ssh/ssh_config',
+    match    => 'PasswordAuthentication',
+    line     => 'PasswordAuthentication no',
+    multiple => 'true'
+}
+file_line { 'ssh_config_2':
+    ensure   => present,
+    path     => '/etc/ssh/ssh_config',
+    match    => 'IdentityFile',
+    line     => 'IdentityFile ~/.ssh/holberton',
+    multiple => 'true'
+}

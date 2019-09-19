e1b7ff7ff0d5ab899ee581259ca78bd43d074950
David Kwan <dwkwan1989@gmail.com>
2019-09-19 19:33:07 +0000

    Change comment

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""queries the Reddit API and returns the number of subscribers
-(not active users, total subscribers) for a given subreddit
+"""queries the Reddit API and prints the titles of the first 10 hot posts
+listed for a given subreddit.
 """
 import requests
 
